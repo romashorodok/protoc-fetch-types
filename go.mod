172031f833b7e3d14fa65f1457c549d3e2913521
romashorodok <108732029+romashorodok@users.noreply.github.com>
2023-12-08 13:46:43 +0200

    feat: generate type alias params

--- go.mod
+++ go.mod
@@ -4,8 +4,5 @@ go 1.21
 
 require (
 	github.com/golang/protobuf v1.5.3
-	google.golang.org/genproto/googleapis/api v0.0.0-20231127180814-3a041ad873d4
 	google.golang.org/protobuf v1.31.0
 )
-
-require google.golang.org/genproto v0.0.0-20231120223509-83a465c0220f // indirect
--- pkg/typealias/typealias.go
+++ pkg/typealias/typealias.go
@@ -5,10 +5,63 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"unicode"
+
+	"google.golang.org/protobuf/types/descriptorpb"
 
 	"github.com/romashorodok/protoc-gen-fetch-types/pkg/proxy"
 	"github.com/romashorodok/protoc-gen-fetch-types/pkg/templatebuilder"
 )
+
+func uppercase(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	firstChar := unicode.ToUpper(rune(s[0]))
+	return string(firstChar) + s[1:]
+}
+
+func lowercase(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	firstChar := unicode.ToLower(rune(s[0]))
+	return string(firstChar) + s[1:]
+}
+
+func tsType(t *descriptorpb.FieldDescriptorProto_Type) string {
+	switch *t {
+	case descriptorpb.FieldDescriptorProto_TYPE_DOUBLE,
+		descriptorpb.FieldDescriptorProto_TYPE_FLOAT,
+		descriptorpb.FieldDescriptorProto_TYPE_INT64,
+		descriptorpb.FieldDescriptorProto_TYPE_UINT64,
+		descriptorpb.FieldDescriptorProto_TYPE_INT32,
+		descriptorpb.FieldDescriptorProto_TYPE_FIXED64,
+		descriptorpb.FieldDescriptorProto_TYPE_FIXED32,
+		descriptorpb.FieldDescriptorProto_TYPE_UINT32,
+		descriptorpb.FieldDescriptorProto_TYPE_SFIXED32,
+		descriptorpb.FieldDescriptorProto_TYPE_SFIXED64,
+		descriptorpb.FieldDescriptorProto_TYPE_SINT32,
+		descriptorpb.FieldDescriptorProto_TYPE_SINT64:
+		return "number"
+	case descriptorpb.FieldDescriptorProto_TYPE_STRING:
+		return "string"
+	case descriptorpb.FieldDescriptorProto_TYPE_BOOL:
+		return "boolean"
+	case descriptorpb.FieldDescriptorProto_TYPE_MESSAGE:
+		return "Object"
+	default:
+		return "any"
+	}
+}
+
+func tsArray(field *descriptorpb.FieldDescriptorProto) bool {
+	return field.GetLabel() == descriptorpb.FieldDescriptorProto_LABEL_REPEATED
+}
+
+func typeAliasName(message *proxy.MessageProxy) string {
+	return fmt.Sprintf("%sPackage%s", message.GetName(), uppercase(message.GetPackageName()))
+}
 
 type TypeAlias struct {
 	ref  *proxy.MessageProxy
@@ -30,17 +83,31 @@ func (s *TypeAlias) tmplStruct() template_TypeAlias {
 	var params []*template_TypeAliasParams
 
 	for _, message := range s.ref.GetFieldsMessages() {
-		name := message.GetName()
-		packageName := message.GetPackageName()
+		var isArray bool
+		if field, err := message.GetOriginField(); err == nil {
+			isArray = tsArray(field)
+		}
 
-		params = append(params, &template_TypeAliasParams{
-			Name: fmt.Sprintf("%sPackage_%s", packageName, name),
-			Type: "string",
-		})
+		params = append(params,
+			&template_TypeAliasParams{
+				Name:  fmt.Sprintf("%s_%s", lowercase(message.GetName()), message.GetPackageName()),
+				Type:  typeAliasName(message),
+				Array: isArray,
+			})
+	}
+
+	for _, field := range s.ref.GetPrimitiveFields() {
+		params = append(params,
+			&template_TypeAliasParams{
+				Name:  field.GetName(),
+				Type:  tsType(field.Type),
+				Array: tsArray(field),
+			},
+		)
 	}
 
 	return template_TypeAlias{
-		Name:       s.ref.GetName(),
+		Name:       typeAliasName(s.ref),
 		Parameters: params,
 	}
 }
